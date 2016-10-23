f1e4ba4581e34d77c446b0000a4c3b0bce1b43e0
lin432 <yinlintao@gmail.com>
2016-10-23 10:38:34 -0400

    Add files via upload

---
+++ Scripts.js
@@ -0,0 +1,15 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function message()
+{
+    var messageStart = "Thank you for your donation.  No matter what, how, when, where, who or why you choose to donate to Women's Habitat the positive effects of your donation will help us to help women in fundamental needs.  Moreover the spinoff or indirect benefits of your donation will also benefit others.  If you wish further information about your donation or indeed our organization and our services please use our ";
+    var linkString = "CONTACTS PAGE";
+    var messageEnd = ".  Please bear in mind though that Women's Habitat may not respond as quickly as we wish.  Again, thank you for your consideration and charitableness towards others.";
+    var linkedString = linkString.link("https://womenshabitat.ca/about/contact/");
+    
+    window.open("Message.html");
+}
+
